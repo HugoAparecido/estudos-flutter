e62f612c987e10d138b98c92748eb70d97af8e35
Hugo Aparecido Gregorio de Almeida <120059026+HugoAparecido@users.noreply.github.com>
2026-02-04 18:37:25 -0300

    criação de testes de componentes

--- S6Dart001/mapas_null_safety.dart
+++ S6Dart001/mapas_null_safety.dart
@@ -1,9 +1,5 @@
 void main() {
   Map<int, String?>? ddds;
 
-  if (ddds != null) {
-    ddds.remove(11);
-  }
-
   ddds = {11: 'São Paulo', 19: 'Campinas', 41: 'Curitiba', 49: null};
 }
---
+++ testes/flutter_application_1/android/app/src/main/kotlin/com/satelite_autopecas/flutter_application_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.satelite_autopecas.flutter_application_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
